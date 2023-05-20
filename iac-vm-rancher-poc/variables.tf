26bbf36bd4374a29489d518c3b2be543e70cb82f
Rodolfo Casanova <6202719+rodolfocasanova@users.noreply.github.com>
2023-05-19 18:44:47 -0600

    fix

---
+++ iac-vm-rancher-poc/variables.tf
@@ -0,0 +1,36 @@
+# Define variables
+variable "instance_name" {
+  description = "Nombre de la instancia de VM"
+  type        = string
+  default     = "rancher-instance"
+}
+
+variable "machine_type" {
+  description = "Tipo de máquina de la instancia de VM"
+  type        = string
+  default     = "n1-standard-1"
+}
+
+variable "zone" {
+  description = "Zona de GCP donde se creará la instancia de VM"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "rancher_image" {
+  description = "Imagen de Docker para Rancher"
+  type        = string
+  default     = "rancher/rancher"
+}
+
+variable "gcp_project_id" {
+  description = "ID del proyecto de GCP"
+  type        = string
+  default     = "training-384005"
+}
+
+variable "gcp_region" {
+  description = "Región de GCP"
+  type        = string
+  default     = "us-central1"
+}
